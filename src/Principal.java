3033c6f55b6820d63a407138aa5fb1919e030e18
jotavezz <joaovitordossantosmendonca@gmail.com>
2025-06-26 17:53:23 -0300

    Atualizando

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,19 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme primeiroFilme = new Filme();
-        primeiroFilme.nome = "Interestelar";
-        primeiroFilme.anoDeLancamento = 2014;
-        primeiroFilme.duracaoEmMinutos = 169;
+        primeiroFilme.setNome("Interestelar");
+        primeiroFilme.setAnoDeLancamento(2014);
+        primeiroFilme.setDuracaoEmMinutos(169);
 
         primeiroFilme.exibeFichaTecnica();
         primeiroFilme.avalia(8);
         primeiroFilme.avalia(10);
         primeiroFilme.avalia(5);
-        System.out.println("A soma das avaliações é: " + primeiroFilme.somaAvaliacoes);
-        System.out.println("O total de avaliações é: " + primeiroFilme.totalDeAvaliacoes);
+        System.out.println("O total de avaliações é: " + primeiroFilme.getTotalDeAvaliacoes());
         System.out.println("A média das avaliações é: " + primeiroFilme.retornaMedia());
+        System.out.println(primeiroFilme.retornaMedia());
+
     }
 }
